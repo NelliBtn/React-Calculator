3057696a30618514587d98a49081dfa6c9414317
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-14 18:53:08 -0500

    create states and pass down as props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import Screen from './components/Screen';
 import Buttons from './components/Buttons';
+import { useState } from 'react';
 
 function App() {
-  const curr_operrand = 0;
-  const prev_operrand = 10;
-  const operator = '+';
+  const [prev_operrand, setPrevOperrand] = useState('');
+  const [curr_operrand, setCurrOperrand] = useState('');
+  const [operator, setOperator] = useState('');
+  const [result, setResult] = useState('');
 
   return (
     <div className="App">
-      <Screen prev_operrand={prev_operrand} curr_operrand={curr_operrand} operator={operator}/>
-      <Buttons />
+      <Screen prev_operrand={prev_operrand} curr_operrand={curr_operrand} operator={operator} result={result}/>
+      <Buttons 
+        prev_operrand={prev_operrand} 
+        setPrevOperrand={setPrevOperrand} 
+        curr_operrand={curr_operrand} 
+        setCurrOperrand={setCurrOperrand} 
+        operator={operator}
+        setOperator={setOperator}
+        setResult={setResult}
+      />
     </div>
   );
 }
