749cbd51c5fb24f82ba7ffe9c7abe937e0930da0
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-14 18:56:36 -0500

    accept props and use them to handle clicks on different buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,7 @@
 import './Buttons.css';
 import Button from './Button';
 
-export default function Buttons() {
+export default function Buttons({ setCurrOperrand, curr_operrand, operator, setOperator, prev_operrand, setPrevOperrand, setResult}) {
 
   const buttons = [
     ['AC', '.', '+'],
@@ -11,6 +11,43 @@ export default function Buttons() {
     ['DEL', 0, '=']
   ];
 
+  // EDGE CASES TO CONSIDER
+  // no more than 1 zero before the dot . (ends up in error)
+  // no more than 12 chars OR text-wrap
+  
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const click = e.target.value;
+    switch(click) {
+      case 'AC':
+        setCurrOperrand('');
+        setOperator('');
+        setPrevOperrand('');
+        setResult('');
+        break;
+      case 'DEL':
+        setCurrOperrand(curr_operrand.slice(0, -1));
+        break;
+      case '+':
+      case '-':
+      case '/':
+      case '*':
+        setPrevOperrand(curr_operrand);
+        setOperator(click);
+        setCurrOperrand('');
+        break;
+      case '=':
+        const result = eval(prev_operrand + operator + curr_operrand);
+        setResult(result);
+        setCurrOperrand('');
+        setOperator('');
+        setPrevOperrand('');
+      default:
+        setCurrOperrand(curr_operrand += click);
+    }
+  }
+
   return(
     <div className="buttons">
       {buttons.flat().map((btn, i) => {
@@ -19,7 +56,7 @@ export default function Buttons() {
             key={i}
             value={btn}
             className={btn === 'AC' || btn === 'DEL' ? 'long-btn' : ''}
-            onClick={() => console.log(`${btn} clicked`)}
+            handleClick={handleClick}
           />
         )
       })}
