51ccb5e359690ce2b8e10653b63e7acabbe3ff60
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-14 18:55:21 -0500

    accept result prop and use it in conditional rendering

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,16 @@
 import './Screen.css'
 
-export default function Screen({prev_operrand, curr_operrand, operator}) {
+export default function Screen({prev_operrand, curr_operrand, operator, result}) {
   return(
     <div className="screen">
-      <p className='prev-operrand'>{prev_operrand} {operator}</p>
-      <p className='curr-operrand'>{curr_operrand}</p>
+      {result ? (
+        <p className='curr-operrand'>{result}</p>
+      ) : (
+        <>
+        <p className='prev-operrand'>{prev_operrand} {operator}</p>
+        <p className='curr-operrand'>{curr_operrand}</p>
+        </>
+      )}
     </div>
   )
 }
