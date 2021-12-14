8cb686d97977d89d7b3fd00109a46f405c59167a
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-14 15:32:13 -0500

    move button component and buttons array from app to buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import './App.css';
 import Screen from './components/Screen';
 import Buttons from './components/Buttons';
-import Button from './components/Button';
 
 function App() {
   const curr_operrand = 0;
   const prev_operrand = 10;
   const operator = '+';
 
-  const buttons = [
-    ['AC', '.', '+'],
-    [1, 2, 3, '-'],
-    [4, 5, 6, '/'],
-    [7, 8, 9, '*'],
-    ['DEL', 0, '=']
-  ];
-
   return (
     <div className="App">
       <Screen prev_operrand={prev_operrand} curr_operrand={curr_operrand} operator={operator}/>
-      <Buttons>
-        <Button />
-      </Buttons>
+      <Buttons />
     </div>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,28 @@
 import './Buttons.css';
+import Button from './Button';
 
 export default function Buttons() {
+
+  const buttons = [
+    ['AC', '.', '+'],
+    [1, 2, 3, '-'],
+    [4, 5, 6, '/'],
+    [7, 8, 9, '*'],
+    ['DEL', 0, '=']
+  ];
+
   return(
-    <div className="buttons"></div>
+    <div className="buttons">
+      {buttons.flat().map((btn, i) => {
+        return (
+          <Button
+            key={i}
+            value={btn}
+            className={btn === 'AC' || btn === 'DEL' ? 'long-btn' : ''}
+            onClick={() => console.log(`${btn} clicked`)}
+          />
+        )
+      })}
+    </div>
   )
 }
