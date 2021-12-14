d4bb11b5ae387fea46f1ec34f870771b287c9322
Nelli Butenko <butenkonelly93@gmail.com>
2021-12-14 14:53:43 -0500

    accept and dicplay props

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,10 @@
 import './Screen.css'
 
-export default function Screen() {
+export default function Screen({prev_operrand, curr_operrand, operator}) {
   return(
-    <div className="screen">0</div>
+    <div className="screen">
+      <p className='prev-operrand'>{prev_operrand} {operator}</p>
+      <p className='curr-operrand'>{curr_operrand}</p>
+    </div>
   )
 }
